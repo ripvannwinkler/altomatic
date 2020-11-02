0bfd1ef2f35a2f175430af9f51715faa10cf403f
Chris Vann <chrisvann01@gmail.com>
2020-11-02 00:02:28 -0600

    add send command, medicated item check

--- Altomatic.UI/Game/ActionManager.cs
+++ Altomatic.UI/Game/ActionManager.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Altomatic.UI.Game.Data;
 using Altomatic.UI.Utilities;
 using Altomatic.UI.ViewModels;
 
@@ -11,8 +13,6 @@ namespace Altomatic.UI.Game
 {
 	public class ActionManager
 	{
-		static readonly SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
-
 		public AppViewModel App { get; }
 
 		public ActionManager(AppViewModel App)
@@ -27,43 +27,65 @@ namespace Altomatic.UI.Game
 
 		public async Task<bool> UseItem(string itemName, string targetName = "<me>")
 		{
-			return await DoAction(() =>
+			return await Task.Run<bool>(async () =>
 			{
 				if (!App.Healer.HasItem(itemName)) return false;
+				if (App.Healer.Player.Buffs.Contains(Buffs.Medicine))
+        {
+					if (Items.Medicines.Contains(itemName)) return false;
+        }
+
 				App.SetStatus($"Using item {itemName} on {targetName}");
+				await App.Healer.SendCommand($"/ja \"{itemName}\" {targetName}", 3000);
 				return true;
 			});
 		}
 
 		public async Task<bool> UseAbility(string abilityName, string targetName = "<me>")
 		{
-			return await DoAction(() =>
+			return await Task.Run<bool>(async () =>
 			{
 				if (!App.Jobs.CanUseAbility(abilityName)) return false;
 				App.SetStatus($"Using ability {abilityName} on {targetName}");
+				await App.Healer.SendCommand($"/ja \"{abilityName}\" {targetName}", 2000);
 				return true;
 			});
 		}
 
 		public async Task<bool> CastSpell(string spellName, string targetName = "<me>")
 		{
-			return await DoAction(() =>
+			return await Task.Run<bool>(async () =>
 			{
 				if (!App.Spells.CanCast(spellName)) return false;
 				App.SetStatus($"Casting {spellName} on {targetName}");
-				return true;
+
+				var casting = false;
+				var completed = false;
+				using var sub = App.Addon.Events.Subscribe(@event =>
+				{
+					if (@event.Type == AddonEventType.CastingStarted) casting = true;
+					if (@event.Type == AddonEventType.CastingCompleted) completed = true;
+					if (@event.Type == AddonEventType.CastingInteruppted) completed = true;
+				});
+
+				var spellInfo = App.Healer.Resources.GetSpell(spellName, 0);
+				await App.Healer.SendCommand($"/ma \"{spellName}\" {targetName}", 3000);
+				var timer = Stopwatch.StartNew();
+
+				while (casting)
+				{
+					if (completed) return true;
+					if (timer.ElapsedMilliseconds > spellInfo.CastTime)
+					{
+						return false;
+					}
+
+					await Task.Delay(100);
+					continue;
+				}
+
+				return false;
 			});
-		}
-
-		protected async Task<bool> DoAction(Func<bool> action)
-		{
-			if (await semaphore.WaitAsync(500))
-			{
-				try { return action.Invoke(); }
-				finally { semaphore.Release(); }
-			}
-
-			return false;
 		}
 	}
 }
---
+++ Altomatic.UI/Game/Data/Items.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altomatic.UI.Game.Data
+{
+	public class Items
+	{
+		public static string[] Medicines =
+		{
+			"Potion Drop",
+			"Hi-Potion Drop",
+			"Hi-Ether Drop",
+			"Pro-Ether",
+			"Hyper Ether",
+			"Max-Potion",
+			"Hyper Potion",
+			"Icarus Wing",
+			"Hermes Quencher",
+			"Black Drop",
+			"Blue Drop",
+			"Clear Drop",
+			"Green Drop",
+			"Purple Drop",
+			"Red Drop",
+			"White Drop",
+			"Yellow Drop",
+			"Dexterity Potion",
+			"Mind Potion",
+			"Intelligence Potion",
+			"Strength Potion",
+			"Agility Potion",
+			"Charisma Potion",
+		};
+	}
+}
