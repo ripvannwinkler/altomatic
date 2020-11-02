170cc85d2e91c829b33933a040ee36923d4797c6
Chris Vann <chrisvann01@gmail.com>
2020-11-02 01:51:53 -0600

    add petrified strategy, pre-check spell access

--- Altomatic.UI/Game/Strategies/PetrifiedStrategy.cs
+++ Altomatic.UI/Game/Strategies/PetrifiedStrategy.cs
@@ -10,14 +10,14 @@ using static EliteMMO.API.EliteAPI;
 
 namespace Altomatic.UI.Game.Strategies
 {
-  public class DoomedStrategy : IGameStrategy
-  {
-    public async Task<bool> ExecuteAsync(AppViewModel app)
-    {
-			if (!app.Spells.CanCast("Cursna")) return false;
-      var healerEntity = app.Healer.Entity.GetLocalPlayer();
-      var members = app.Monitored.Party.GetPartyMembers();
-      var candidates = new List<PartyMember>();
+	public class PetrifiedStrategy : IGameStrategy
+	{
+		public async Task<bool> ExecuteAsync(AppViewModel app)
+		{
+			if (!app.Spells.CanCast("Stona")) return false;
+			var healerEntity = app.Healer.Entity.GetLocalPlayer();
+			var members = app.Monitored.Party.GetPartyMembers();
+			var candidates = new List<PartyMember>();
 
 			for (var i = 0; i < 18; i++)
 			{
@@ -28,23 +28,23 @@ namespace Altomatic.UI.Game.Strategies
 
 				if (distance < 21 &&
 						member.Active > 0 &&
-						app.Buffs.HasAny(member.Name, Buffs.Doom, Buffs.Curse))
+						app.Buffs.HasAny(member.Name, Buffs.Petrification))
 				{
 					var player = app.Players.SingleOrDefault(x => x.Name == member.Name);
 					if (player?.IsEnabled ?? false) candidates.Add(member);
 				}
 			}
 
-			if (candidates.Any())
-      {
+			if (candidates.Any() && candidates.Min(c => c.CurrentHPP) > 75)
+			{
 				var target = candidates.First();
-				if (await app.Actions.CastSpell("Cursna", target.Name))
-        {
+				if (await app.Actions.CastSpell("Stona", target.Name))
+				{
 					return true;
-        }
-      }
+				}
+			}
 
 			return false;
 		}
-  }
+	}
 }
