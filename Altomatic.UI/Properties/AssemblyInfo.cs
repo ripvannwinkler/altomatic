b8fa5648f11482bc29dec92d94b776d2e01d03d8
Chris Vann <chrisvann01@gmail.com>
2020-11-06 02:08:51 -0600

    fix version number issues

--- Altomatic.UI/Properties/AssemblyInfo.cs
+++ Altomatic.UI/Properties/AssemblyInfo.cs
@@ -52,5 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.*")]
 [assembly: AssemblyInformationalVersion("1.0.3-beta.1")]
