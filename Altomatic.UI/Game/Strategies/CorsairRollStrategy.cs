c617fa1a3ac6f8a0b0cf97171c6a951fba29b521
Chris Vann <chrisvann01@gmail.com>
2020-11-10 22:36:02 -0600

    cleanup

--- Altomatic.UI/Game/Strategies/CorsairRollStrategy.cs
+++ Altomatic.UI/Game/Strategies/CorsairRollStrategy.cs
@@ -111,7 +111,7 @@ namespace Altomatic.UI.Game.Strategies
 			{
 				if (player.IsRequiredForRolls)
 				{
-					if (player.DistanceFromHealer > 8 || player.CurrentHp == 0)
+					if (player.DistanceFromHealer > 8 || player.CurrentHp < 1)
 					{
 						outOfRange++;
 					}
